6f59cabd38da3f0c19fd27617001a7ff26dbb0b4
Ollie Etherington <ollie@etherington.xyz>
2021-12-30 12:05:10 +0000

    Code coverage report for commit 907eb9996fd96a29041455151d4a32cecff2462e

---
+++ docs/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+{"link":"test.5a4b29755cfe6017/index.html","title":"test","summary_name":"test","covered_class":"lineCov","covered":"94.3","covered_lines":"4770","uncovered_lines":"291","total_lines" : "5061"},
+], merged_files:[]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "test", "date" : "2021-12-30 12:05:09", "instrumented" : 5061, "covered" : 4770,};
